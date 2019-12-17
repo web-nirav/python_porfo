3f43ecc6ce06ebdd08a309bdccf7ca3e4b4fb2dc
nirav.prajapati <nirav.prajapati@indianic.com>
2019-12-17 13:40:48 +0530

    initial commit for basic porfolio set up

---
+++ server.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect
+import csv 
+
+app = Flask(__name__)
+
+# print(__name__)
+
+# Remember that this is the decorator function as we learned 
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+# Form submitting of contact page
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database.'
+    else:
+        return 'Something went wrong. Try again!'
+    # error = None
+    # if request.method == 'POST':
+    #     if valid_login(request.form['username'],
+    #                    request.form['password']):
+    #         return log_the_user_in(request.form['username'])
+    #     else:
+    #         error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    # return render_template('login.html', error=error)
+
+# @app.route('/<username>/<int:post_id>')
+# def hello_world(username=None, post_id=None):
+#     return render_template('index.html', name=username, post_id=post_id)    
